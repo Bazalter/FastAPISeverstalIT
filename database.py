306ccc69a0b7cf93f0318cdfb1d30b3a6eed1315
Bazalter <egor-zaikin3@yandex.ru>
2024-05-24 15:30:33 +0300

    changed models

---
+++ database.py
@@ -0,0 +1,8 @@
+from sqlalchemy.engine import create_engine
+from sqlalchemy.orm import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    pass
+
+
--- models.py
+++ models.py
@@ -1,17 +1,14 @@
-from sqlalchemy import Column, Float, Integer, String, DateTime
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import String, Float, Integer, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
 from datetime import datetime
-
-
-class Base(DeclarativeBase):
-    pass
+from database import Base
 
 
 class Roll(Base):
     __tablename__ = 'rolls'
 
-    id = Column(Integer, primary_key=True, index=True)
-    length = Column(Float, nullable=False)
-    weight = Column(Float, nullable=False)
-    date_added = Column(DateTime, default=datetime.utcnow)
-    date_removed = Column(DateTime, nullable=False)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    length: Mapped[float] = mapped_column(Float, nullable=False)
+    weight: Mapped[float] = mapped_column(Float, nullable=False)
+    date_added: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    date_removed: Mapped[datetime] = mapped_column(DateTime, nullable=False)
